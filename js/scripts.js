189eb92b1ce12fa7fe5bab851422c59b2c54f417
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-02-03 14:37:52 -0300

    Criado menu de criar tarefas.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Elements
+const btnCreateFirstTask = document.querySelector("#btn-create");
+
+// Functions
+const showMenuCreateTask = () => {
+
+};
+
+// Events
+btnCreateFirstTask.addEventListener("click", showMenuCreateTask);
